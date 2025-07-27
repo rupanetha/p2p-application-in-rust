d0fa567adf61c07f187a5917f524345254c404a9
Rupa Netha <iamsairupa06@gmail.com>
2025-07-27 21:09:37 +0530

    add mdns to main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use libp2p::futures::StreamExt;
+use libp2p::mdns::tokio::Tokio;
 use libp2p::ping::Config;
 use libp2p::request_response::json;
 use libp2p::swarm::{NetworkBehaviour, SwarmEvent};
-use libp2p::{noise, ping, request_response, tcp, yamux, Multiaddr, StreamProtocol};
+use libp2p::{mdns, noise, ping, request_response, tcp, yamux, PeerId, StreamProtocol};
 use serde::{Deserialize, Serialize};
 use std::time::Duration;
 use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::{io, select};
-
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct MessageRequest {
@@ -23,15 +23,11 @@ struct MessageResponse {
 struct ChatBehaviour {
     ping: ping::Behaviour,
     messaging: json::Behaviour<MessageRequest, MessageResponse>,
+    mdns: mdns::Behaviour<Tokio>,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    dotenv::dotenv().ok(); 
-
-    let port = std::env::var("CHAT_P2P_PORT").unwrap_or(String::from("9999")).parse::<u16>()?;
-    let peer: Multiaddr = std::env::var("CHAT_PEER")?.parse()?;
-
     let mut swarm =
         libp2p::SwarmBuilder::with_new_identity()
             .with_tokio()
@@ -40,7 +36,7 @@ async fn main() -> anyhow::Result<()> {
                 noise::Config::new,
                 yamux::Config::default,
             )?
-            .with_behaviour(|_key_pair| {
+            .with_behaviour(|key_pair| {
                 Ok(
                     ChatBehaviour {
                         ping: ping::Behaviour::new(Config::new().with_interval(Duration::from_secs(10))),
@@ -51,35 +47,27 @@ async fn main() -> anyhow::Result<()> {
                             )],
                             request_response::Config::default(),
                         ),
+                        mdns: mdns::Behaviour::new(mdns::Config::default(), key_pair.public().to_peer_id())?,
                     }
                 )
             })?
             .with_swarm_config(|config| { config.with_idle_connection_timeout(Duration::from_secs(30)) })
             .build();
 
-    swarm.listen_on(format!("/ip4/0.0.0.0/tcp/{}", port).parse()?)?;
-    swarm.dial(peer.clone())?;
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
     println!("Peer ID: {:?}", swarm.local_peer_id());
 
     let mut stdin = BufReader::new(io::stdin()).lines();
 
-    let mut target_peer_id = None;
     loop {
         select! {
             event = swarm.select_next_some() => match event {
                 SwarmEvent::NewListenAddr { address, .. } => {
                     println!("Listening on {:?}", address);
                 }
-                SwarmEvent::ConnectionClosed {..} => {
-                    target_peer_id = None;
-                }
                 SwarmEvent::ConnectionEstablished{peer_id, ..} => {
                     println!("Connection established with peer {:?}!", peer_id);
-                    if target_peer_id.is_none() {
-                        target_peer_id = Some(peer_id);
-                    }
-                    swarm.add_peer_address(peer_id, peer.clone());
                 }
                 SwarmEvent::Behaviour(event) => match event {
                     ChatBehaviourEvent::Ping(event) => {
@@ -104,12 +92,23 @@ async fn main() -> anyhow::Result<()> {
                             println!("InboundFailure from {:?} to {:?}: {:?}", peer, request_id, error);
                         },
                         request_response::Event::ResponseSent{ .. } => {},
+                    },
+                    ChatBehaviourEvent::Mdns(event) => match event {
+                        mdns::Event::Discovered(new_peers) => {
+                            for (peer_id, addr) in new_peers {
+                                println!("Discovered {peer_id} at {addr}!");
+                                swarm.dial(addr.clone())?;
+                                swarm.add_peer_address(peer_id, addr);
+                            }
+                        }
+                        mdns::Event::Expired(_) => {}
                     }
                 }
                 _ => {}
             },
             Ok(Some(line)) = stdin.next_line() => {
-                if let Some(peer_id) = target_peer_id {
+                let peers: Vec<PeerId> = swarm.connected_peers().copied().collect();
+                for peer_id in peers {
                     swarm.behaviour_mut().messaging.send_request(&peer_id, MessageRequest { message: line.clone() });
                     println!("{} {line:?}", swarm.local_peer_id());
                 }
@@ -117,4 +116,3 @@ async fn main() -> anyhow::Result<()> {
         }
     }
 }
-

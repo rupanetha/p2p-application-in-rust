c069ee4ea2ed6c784e595b5a5f2dfca55edc7076
Rupa Netha <iamsairupa06@gmail.com>
2025-07-29 01:14:21 +0530

    add relays

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use libp2p::ping::Config;
 use libp2p::request_response::json;
 use libp2p::swarm::behaviour::toggle::Toggle;
 use libp2p::swarm::{NetworkBehaviour, SwarmEvent};
-use libp2p::{identify, kad, mdns, noise, ping, request_response, tcp, yamux, Multiaddr, PeerId, StreamProtocol};
+use libp2p::{autonat, dcutr, identify, kad, mdns, noise, ping, relay, request_response, tcp, yamux, Multiaddr, PeerId, StreamProtocol};
 use serde::{Deserialize, Serialize};
 use std::env;
 use std::time::Duration;
@@ -32,6 +32,10 @@ struct ChatBehaviour {
     mdns: Toggle<mdns::Behaviour<Tokio>>,
     identify: identify::Behaviour,
     kademlia: kad::Behaviour<MemoryStore>,
+    autonat: autonat::Behaviour,
+    relay_server: relay::Behaviour,
+    relay_client: relay::client::Behaviour,
+    dcutr: dcutr::Behaviour,
 }
 
 #[tokio::main]
@@ -48,7 +52,11 @@ async fn main() -> anyhow::Result<()> {
                 noise::Config::new,
                 yamux::Config::default,
             )?
-            .with_behaviour(|key_pair| {
+            .with_relay_client(
+                noise::Config::new,
+                yamux::Config::default,
+            )?
+            .with_behaviour(|key_pair, relay_client| {
                 let mdns = if mdns_enabled {
                     Toggle::from(Some(mdns::Behaviour::new(mdns::Config::default(), key_pair.public().to_peer_id())?))
                 } else {
@@ -75,6 +83,10 @@ async fn main() -> anyhow::Result<()> {
                             MemoryStore::new(key_pair.public().to_peer_id()),
                             kad_config,
                         ),
+                        autonat: autonat::Behaviour::new(key_pair.public().to_peer_id(), autonat::Config::default()),
+                        relay_server: relay::Behaviour::new(key_pair.public().to_peer_id(), relay::Config::default()),
+                        relay_client,
+                        dcutr: dcutr::Behaviour::new(key_pair.public().to_peer_id()),
                     }
                 )
             })?
@@ -144,8 +156,8 @@ async fn main() -> anyhow::Result<()> {
                         mdns::Event::Discovered(new_peers) => {
                             for (peer_id, addr) in new_peers {
                                 println!("Discovered {peer_id} at {addr}!");
-                                swarm.dial(addr.clone())?;
-                                swarm.add_peer_address(peer_id, addr);
+                                swarm.add_peer_address(peer_id, addr.clone());
+                                swarm.behaviour_mut().kademlia.add_address(&peer_id, addr);
                             }
                         }
                         mdns::Event::Expired(_) => {}
@@ -153,8 +165,17 @@ async fn main() -> anyhow::Result<()> {
                     ChatBehaviourEvent::Identify(event) => match event {
                         identify::Event::Received{connection_id,peer_id,info  } => {
                             println!("New identify received: {peer_id} - {info:?}");
+                            let is_relay = info.protocols.iter().any(|protocol| *protocol == relay::HOP_PROTOCOL_NAME);
+                            
                             for addr in info.listen_addrs {
-                                swarm.behaviour_mut().kademlia.add_address(&peer_id, addr);
+                                swarm.behaviour_mut().kademlia.add_address(&peer_id, addr.clone());
+                                
+                                if is_relay
+                                {
+                                    let listen_addr = addr.clone().with_p2p(peer_id).unwrap().with(Protocol::P2pCircuit);
+                                    println!("Trying to listen on {:?}", listen_addr);
+                                    swarm.listen_on(listen_addr)?;
+                                }
                             }
                         },
                         identify::Event::Sent{ .. } => {},
@@ -176,6 +197,26 @@ async fn main() -> anyhow::Result<()> {
                         kad::Event::RoutablePeer{ .. } => {},
                         kad::Event::PendingRoutablePeer{ .. } => {},
                         kad::Event::ModeChanged{ .. } => {}},
+                    ChatBehaviourEvent::Autonat(event) => match event {
+                        autonat::Event::InboundProbe(event) => {
+                            println!("Inbound probe: {:?}", event);
+                        },
+                        autonat::Event::OutboundProbe(event) => {
+                            println!("Outbound probe: {:?}", event);
+                        },
+                        autonat::Event::StatusChanged{old,new} => {
+                            println!("Status changed from {:?} to {:?}", old, new);
+                        }
+                    },
+                    ChatBehaviourEvent::RelayServer(event) => {
+                        println!("Relay server: {:?}", event);
+                    },
+                    ChatBehaviourEvent::RelayClient(event) => {
+                        println!("Relay client: {:?}", event);
+                    },
+                    ChatBehaviourEvent::Dcutr(event) => {
+                        println!("Dcutr: {:?}", event);
+                    },
                 }
                 _ => {}
             },
